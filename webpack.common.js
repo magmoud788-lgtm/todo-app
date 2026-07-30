a168bab2b3442306e1532d4179d5f60c25e4fa30
magmoud <magmoud788@gmail.com>
2026-07-30 14:09:53 +0400

    deploy todo app

--- webpack.common.js
+++ webpack.common.js
@@ -6,7 +6,7 @@ export default {
     entry: './src/index.js',
     output: {
         filename: 'main.js',
-        path: path.resolve(import.meta.dirname, 'dist'),
+        path: path.resolve(import.meta.dirname, 'docs'),
         clean: true,
     },
     plugins: [
